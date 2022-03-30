7df16b14e4a4230b40bf1c4e91aa95f57c621bff
Bruce Liu <brucelgq.liu@gmail.com>
2022-03-30 16:49:57 +1300

    lbp histogram.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -7,6 +7,10 @@ from dotenv import find_dotenv, load_dotenv
 import pandas as pd
 import os
 import os.path as osp
+import skimage.io
+import skimage.color
+import skimage.filters
+from PIL import Image
 
 def draw_circles(img,circles, title):
     color = (255, 0, 0)
@@ -23,7 +27,70 @@ def draw_circles(img,circles, title):
     plt.imshow(img)
     plt.show()
 
-def detect_outside_circle(file_name, blur_ksize=25):
+
+def detect_circles(pil_image, title):
+    open_cv_image = np.array(pil_image.convert('RGB'))
+    # Convert RGB to BGR
+    img = open_cv_image[:, :, ::-1].copy()
+    #     img = cv2.cv.CreateImageHeader(pimg.size,cv2.IPL_DEPTH_8U,3)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #     gray = np.asarray(img)
+    # plt.imshow(gray)
+    # plt.show()
+    blurred = cv2.medianBlur(gray, 25)  # cv2.bilateralFilter(gray,10,50,50)
+
+    minDist = 500
+    param1 = 30  # 500
+    param2 = 50  # 200 #smaller value-> more false circles
+    minRadius = 0
+    maxRadius = 0  # 10
+
+    # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2,
+                               minRadius=minRadius, maxRadius=maxRadius)
+    #     circles = cv2.HoughCircles(blurred,cv2.HOUGH_GRADIENT,1,60,param1=50,param2=30,minRadius=0,maxRadius=0)
+    #     circles = cv2.HoughCircles(blurred,cv2.HOUGH_GRADIENT,1,minDist,param1=50,param2=30,minRadius=0,maxRadius=0)
+
+    # print('circles are ', circles)
+    # Red color in BGR
+    #     color = (0, 0, 255)
+    color = (255, 0, 0)
+    # Line thickness of -1 px
+    thickness = 10
+
+    # Using cv2.circle() method
+    # Draw a circle of red color of thickness -1 px
+    #     image = cv2.circle(image, center_coordinates, radius, color, thickness)
+
+    if circles is not None:
+        len_circle = len(circles[0])
+        print('The number of circle is ', len_circle)
+        # if len_circle > 1:
+        # head, tail = os.path.split(file_name)
+        draw_circles(img, circles, title)
+
+        # circles = np.uint16(np.around(circles))
+        # num = 1
+        # for i in circles[0, :]:
+        #     print('i is ', i)
+        #     num = num + 1
+        #     cv2.circle(img, (i[0], i[1]), i[2], color, num*10)
+        #     cv2.circle(img, (i[0], i[1]), 20, color, 3)
+    #             if if_inner:
+    #                 cv2.circle(img, (i[0], i[1]), i[2]-150, color, num * thickness)
+
+    # Show result for testing:
+    # cv2.imshow('img', img)
+    #     plt.title(title)
+    #     plt.imshow(img)
+    #     plt.show()
+    else:
+        print('Empty : ', title)
+        draw_circles(pil_image, circles, 'Empty : '+ title)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+def detect_outside_circle(file_name, blur_ksize=25, draw_circle=True):
     img = cv2.imread(file_name)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred = cv2.medianBlur(gray, blur_ksize)  # cv2.bilateralFilter(gray,10,50,50)
@@ -49,34 +116,89 @@ def detect_outside_circle(file_name, blur_ksize=25):
         print('The number of circle is ', len_circle)
         # if len_circle > 1:
         head, tail = os.path.split(file_name)
-        draw_circles(img, circles, tail)
+        if draw_circle:
+            draw_circles(img, circles, tail)
     else:
         _, tail = os.path.split(file_name)
-        return detect_outside_circle(file_name, 5)
+        return detect_outside_circle(file_name, 5, draw_circle)
         # plt.title('Empty ' + tail)
         # plt.imshow(img)
         # plt.show()
         # print(file_name)
-    return circles, len_circle
+    return circles, len_circle, gray
 
-def detect_circle(file_name):
-    img = cv2.imread(file_name, 0)
-    # print(img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.medianBlur(gray, 5)
-    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 5000,
-                              param1=50, param2=30, minRadius=0, maxRadius=0)
-    circles = np.uint16(np.around(circles))
-    print(circles)
-    for i in circles[0, :]:
-        # draw the outer circle
-        cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        # draw the center of the circle
-        cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-    # plt.title('Empty ' + tail)
-    plt.imshow(cimg)
-    plt.show()
+def detect_inner_ring(file_name):
+    # load the image
+    image = skimage.io.imread(file_name)
+    # image = skimage.io.imread('../data/raw/dn/220207a_C1.bmp')
+    # fig, ax = plt.subplots()
+    # plt.imshow(image)
+    # plt.show()
+    # convert the image to grayscale
+    gray_image = skimage.color.rgb2gray(image)
+
+    # blur the image to denoise
+    blurred_image = skimage.filters.gaussian(gray_image, sigma=1.0)
+
+    # fig, ax = plt.subplots()
+    # plt.imshow(blurred_image, cmap='gray')
+    # plt.show()
+
+    t = skimage.filters.threshold_otsu(blurred_image)
+    # print('Found automatic threshold t = {}.'.format(t))
+    # create a mask based on the threshold
+    # before 0.3
+    # t = 0.3
+    binary_mask = blurred_image > t
+
+    # fig, ax = plt.subplots()
+    # plt.imshow(binary_mask, cmap='gray')
+    # plt.show()
+
+    pixels = np.where(binary_mask, gray_image, blurred_image)
+
+    # Save resulting image
+    result = Image.fromarray(pixels)
+    # result.save('result.png')
+    # plt.imshow(result)
+    # plt.show()
+    _, tail = os.path.split(file_name)
+    # return detect_outside_circle(file_name, 5)
+    detect_circles(result, tail)
+
+
+def getLBPimage(image):
+    '''
+    == Input ==
+    gray_image  : color image of shape (height, width)
+
+    == Output ==
+    imgLBP : LBP converted image of the same shape as
+    '''
+
+    ### Step 0: Step 0: Convert an image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    imgLBP = np.zeros_like(gray_image)
+    neighboor = 3
+    for ih in range(0, image.shape[0] - neighboor):
+        for iw in range(0, image.shape[1] - neighboor):
+            ### Step 1: 3 by 3 pixel
+            img = gray_image[ih:ih + neighboor, iw:iw + neighboor]
+            center = img[1, 1]
+            img01 = (img >= center) * 1.0
+            img01_vector = img01.T.flatten()
+            # it is ok to order counterclock manner
+            # img01_vector = img01.flatten()
+            ### Step 2: **Binary operation**:
+            img01_vector = np.delete(img01_vector, 4)
+            ### Step 3: Decimal: Convert the binary operated values to a digit.
+            where_img01_vector = np.where(img01_vector)[0]
+            if len(where_img01_vector) >= 1:
+                num = np.sum(2 ** where_img01_vector)
+            else:
+                num = 0
+            imgLBP[ih + 1, iw + 1] = num
+    return (imgLBP)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -98,18 +220,43 @@ if __name__ == '__main__':
     # for file in empty_files:
     #     detect_outside_circle(file, 5)
     # exit(0)
+
+
     p_dn = Path(str(project_dir) + '/data/raw/dn').resolve()
     p_pc = Path(str(project_dir) + '/data/raw/pc').resolve()
+
+
+
+    # exit(0)
+
     circle_info = []
     for x in p_dn.iterdir():
         # print(x)
         circles, num_cirlces = detect_outside_circle(str(x))
         circle_info.append([circles, num_cirlces, str(x)])
+        detect_inner_ring(str(x))
+    # exit(0)
     for x in p_pc.iterdir():
         # print(x)
         circles, num_cirlces = detect_outside_circle(str(x))
         circle_info.append([circles, num_cirlces, str(x)])
+        detect_inner_ring(str(x))
 
     print(circle_info)
+
+    # circle_info = []
+    for x in p_dn.iterdir():
+        # print(x)
+        # circles, num_cirlces = detect_outside_circle(str(x))
+        # circle_info.append([circles, num_cirlces, str(x)])
+        detect_inner_ring(str(x))
+    # exit(0)
+    for x in p_pc.iterdir():
+        # print(x)
+        # circles, num_cirlces = detect_outside_circle(str(x))
+        # circle_info.append([circles, num_cirlces, str(x)])
+        detect_inner_ring(str(x))
+
+    # print(circle_info)
     # get_all_datasets(Path(str(project_dir) + '/raw/dn').iterdir())
 
